220c666abf5440ef2f8e9471cf3596e5e3bbd0d2
Lusca Lima <lusca.lima@outlook.com>
2021-05-15 17:42:02 -0300

    creation of feedback service

--- src/views/Feedbacks/index.vue
+++ src/views/Feedbacks/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>Feedbacks</h1>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <TheHeaderLogged />
+  </div>
+
+  <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">Feedbacks</h1>
+    <p class="text-lg text-center text-gray-800 font-regular">
+      Detalhes de todos os feedbacks recebidos.
+    </p>
+  </div>
 </template>
 
 <script>
+// Components
+import TheHeaderLogged from '../../components/TheHeaderLogged'
+
 export default {
-  name: 'Feedbacks'
+  name: 'Feedbacks',
+
+  components: {
+    TheHeaderLogged
+  }
 }
 </script>
